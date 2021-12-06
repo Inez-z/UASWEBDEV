c1837d6d77e9307b7b5a33982752a3bb0ccce5f6
Inez-z <74892336+Inez-z@users.noreply.github.com>
2021-12-06 10:40:28 +0700

    Aboout

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,43 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>About Us</title>
-</head>
-<body>
-</body>
+    <head>
+        <title>About</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    </head>
+
+        <body>
+            <nav>
+                <!--logo----------------->
+                    <img src="image/logo.png" class="logo">
+                <!--menu----------------->
+                    <ul>
+                        <li><a href="#" class="nav-active">Home</a></li>
+                        <li><a href="#">Category</a></li>
+                        <li><a href="#">About</a></li>
+                        <div class="search-container">
+                            <form action="/action_page.php">
+                              <input type="text" placeholder="Search.." name="search" size="35">
+                            </form>
+                          </div>
+                        <li><a href="#"><i class="fa fa-search"></i></a></li>
+                        <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+                        <li><a href="#"><i class="fa fa-user"></i></a></li>
+                    </ul>
+                </nav>
+                <div class="bg">
+                    <img src="image/about_header.png">
+                </div>
+                <div class="desc">
+                    <img src="image/about_desc.png">
+                </div>
+                <div class="box-abt">
+                    <img src="image/Vision.png">
+                </div>
+                <div class="box-team">
+                    <img src="image/Our Team.png">
+                </div>
+        </body>
