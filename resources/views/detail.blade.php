d85cbe6bb1a050565c73f7db2bccf84e72b549b2
ZefanyaOkta <zefanyaoktaviana@gmail.com>
2022-01-06 19:12:50 +0700

    .

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -17,7 +17,7 @@
         <section>
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
                     <!-- {{-- diganti logo --}} -->
-                    <a class="navbar-brand" href="http://127.0.0.1:8000/">
+                    <a class="navbar-brand" href="/welcome">
                         <img src={{asset('image/logo.png')}} width="30" height="30" alt="">
 
                       </a>
@@ -28,7 +28,7 @@
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                       <ul class="navbar-nav mr-auto">
                         <li class="nav-item active">
-                          <a class="nav-link" href="http://127.0.0.1:8000/">Home <span class="sr-only">(current)</span></a>
+                          <a class="nav-link" href="/welcome">Home <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
                           <a class="nav-link" href="#">Link</a>
--- resources/views/women.blade.php
+++ resources/views/women.blade.php
@@ -17,7 +17,7 @@
     <section>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             {{-- diganti logo --}}
-            <a class="navbar-brand" href="http://127.0.0.1:8000/">
+            <a class="navbar-brand" href="/welcome">
                 <img src="../image/logo.png" width="30" height="30" alt="">
               </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -27,7 +27,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                  <a class="nav-link" href="http://127.0.0.1:8000/">Home <span class="sr-only">(current)</span></a>
+                  <a class="nav-link" href="/welcome">Home <span class="sr-only">(current)</span></a>
                 </li>
                 {{-- <li class="nav-item">
                   <a class="nav-link" href="#">Link</a>
