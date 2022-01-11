b305e1a3fb9f64c68b1a5a9caaca137944ba9930
Inez-z <74892336+Inez-z@users.noreply.github.com>
2022-01-11 15:57:07 +0700

    Update posts.blade.php

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -66,7 +66,7 @@
 
             </div>
           </nav>
-        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        {{-- <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
           <div class="carousel-inner">
             <div class="carousel-item active">
               <img class="d-block w-100" src="image/satu.png" alt="First slide">
@@ -77,8 +77,8 @@
             <div class="carousel-item">
               <img class="d-block w-100" src="image/Christmas.png" alt="Third slide">
             </div>
-          </div>
-          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          </div> --}}
+          {{-- <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
           </a>
@@ -86,7 +86,7 @@
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
           </a>
-        </div>
+        </div> --}}
     </section>
 
 <!-- Product ----------------->
