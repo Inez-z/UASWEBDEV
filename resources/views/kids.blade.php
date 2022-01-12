b38930ebd9e20db688aab5e6a0d87500bd726a3f
Inez-z <74892336+Inez-z@users.noreply.github.com>
2022-01-13 00:50:03 +0700

    rpkedollar

--- resources/views/kids.blade.php
+++ resources/views/kids.blade.php
@@ -125,7 +125,7 @@
                 <img src="../image/fotoproduk/{{$i->J_KODE}}" alt="kids1">
             </a>
             <div class="productname"><a href="{{ url('kids/detail/'.$i->J_KODE) }}">{{ $i->J_WARNA }} {{ $i->J_MERK }}</a></div>
-            <div class="productprice"> Rp. {{$i->J_HARGA}}</div>
+            <div class="productprice"> $ {{$i->J_HARGA}}</div>
             </div>
         <div>
     @endforeach
