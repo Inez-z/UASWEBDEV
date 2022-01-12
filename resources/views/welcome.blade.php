1cf5519ec2b3bcd3729efda646501c35244f5f5a
vanya vimala <vanyavimala2015@gmail.com>
2022-01-13 00:06:41 +0700

    ubah warna link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -146,7 +146,7 @@
           <div class="product-text">
               <p></p>
               <p>Our Collections</p>
-              <p><a href="http://127.0.0.1:8000/bestseller">Best Seller</a>  | <a href="/welcome/lowprice">Low Price</a> | <a href="/welcome/highprice">High Price</a></p>
+              <p><a style="color:black" href="http://127.0.0.1:8000/bestseller">Best Seller</a>  | <a style="color:black" href="/welcome/lowprice">Low Price</a> | <a style="color:black" href="/welcome/highprice">High Price</a></p>
 
           </div>
 
@@ -288,7 +288,7 @@
 <div class="footer-home">
     <p class="tulisankanan">WATCHOUT <br><br><br> Address <br> Store & Office <br> Made, Surabaya <br><br><br>Office Hour<br>Monday - Sunday <br> 10.00-18.00 </p>
     <p class="tulisantengah">GET IN TOUCH<br><br><br>Customer Service<br>4452244<br><br>Phone<br>1474832<br><br>Service Center<br>3593748</p>
-    <p class="tulisankiri">SOCIAL MEDIA<br><br><br>Instagram<br><a href="https://www.instagram.com/danielwellington/">@watchout_co</a><br><br>Facebook<br><a href="https://www.facebook.com/">watchout_global</a><br><br>Twitter<br><a href="https://twitter.com/Twitter">watchout_global</a></p>
+    <p class="tulisankiri">SOCIAL MEDIA<br><br><br>Instagram<br><a style="color:white" href="https://www.instagram.com/danielwellington/">@watchout_co</a><br><br>Facebook<br><a style="color:white" href="https://www.facebook.com/">watchout_global</a><br><br>Twitter<br><a style="color:white" href="https://twitter.com/Twitter">watchout_global</a></p>
   </div>
 
   <button class="btnScrollTop" id="btnScrollTop" onclick="scrollToTop()">
